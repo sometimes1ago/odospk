7042d307e887b52f12ecf813f7b9638f92b17d8a
sometimes1ago <achudaev17@gmail.com>
2022-02-25 10:24:55 +0600

    sort control markuped and styled

--- panel/requests/js/sortControl.js
+++ panel/requests/js/sortControl.js
@@ -1,13 +1,13 @@
-const sortOpener = document.querySelector('.queries-controls__sort-btn');
-const sortContainer = document.querySelector('.queries-controls__sort');
-const sortCloser = document.querySelector('.queries-controls__sort-closer');
+const sortOpener = document.querySelector('.query-control__sort-btn');
+const sortContainer = document.querySelector('.query-control__sort');
+const sortCloser = document.querySelector('.query-control__sort-closer');
 
 sortOpener.addEventListener('click', () => {
-    sortOpener.classList.toggle('queries-controls__sort-btn__active');
-    sortContainer.classList.toggle('queries-controls__sort__visible');
+    sortOpener.classList.toggle('query-control__sort-btn__active');
+    sortContainer.classList.toggle('query-control__sort-visible');
 });
 
 sortCloser.addEventListener('click', () => {
-    sortOpener.classList.toggle('queries-controls__sort-btn__active');
-    sortContainer.classList.toggle('queries-controls__sort__visible');
+    sortOpener.classList.toggle('query-control__sort-btn__active');
+    sortContainer.classList.toggle('query-control__sort-visible');
 });
